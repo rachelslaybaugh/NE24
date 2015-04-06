4c117d07ef9ea6af0a45b1091600b749a39e1077
Danny Weitekamp <dweitekamp@berkeley.edu>
2015-04-06 15:02:05 -0700

    added readme and documentation

--- documentation/EstNumJellies.py
+++ documentation/EstNumJellies.py
@@ -43,6 +43,107 @@ class NumJellyEstimator:
     ## Set the world population
     # \param people integer number of people on earth
     def set_world_pop(self, people):
+#!/home/Install/anaconda/bin/python
+
+import sys
+
+## This class estimates the number of jelly beans in the world using input data
+#  determined to be correlated to this result.
+class NumJellyEstimator:
+
+    ## Instantiating the class initializes some variables.
+    def __init__(self):
+
+        # The number of jelly beans in the world is correlated to the fraction
+        # of land used for sugar, the world population, and the fraction of 
+        # people who like the color pink.
+        ## Fraction of land used for growing sugar
+        self.fracLand4Sugar = 0.0
+        ## World population
+        self.worldPop = 0
+        ## Scaling constant used in estimate
+        self.scalingConst = 1e-1
+        ## Fraction of people who love the color pink.
+        self.fracPplLovingPink = 0.0
+
+
+    ## Set the fraction of land used for sugar.
+    def set_land_frac_for_sugar(self, frac):
+
+        # Make sure we've got a float.
+        frac = float(frac) 
+
+        # Check that the value is between zero and one.
+        if ((frac <= 0.0) or (frac >= 1.0)):
+            print "\nError: Fraction of land used for sugar must be between"\
+                  +" 0 and 1.\n"
+            sys.exit()
+
+        # Store the fraction.
+        self.fracLand4Sugar = frac
+
+
+    ## Set the world population
+    # \param number of people in the world(integer)
+    def set_world_pop(self, people):
+
+        # Make sure we've got an int.
+        people = int(people)
+
+        # Check that the value is between zero and 1e20.
+	assert (people >=0) and (people <=1e20), "Wrong number of people"
+
+	
+
+        if ((people <= 0) or (people >= 1e20)):
+            print "\nError: World population must be between 0 and 1e20.\n"
+            sys.exit()
+
+        # Store the fraction.
+        self.worldPop = people
+
+
+    ## Set the fraction of people who love the color pink.
+    def set_frac_ppl_loving_pink(self, frac):
+
+        # Make sure we've got a float.
+        frac = float(frac)
+
+	assert type(frac) is float, "Wrong type"
+
+        # Store the fraction.
+        self.fracPplLovingPink = frac
+
+
+    ## Return the scaling constant so the user can check it if they want.
+    def get_scaling_const(self):
+
+        return self.scalingConst
+
+
+    ## Estimate the number of jelly beans in the world given the world's population, fraction of land used for suger, and a scaling constant. Prints error if returns 0.
+    def compute_Njelly_est(self):
+
+        n = self.fracLand4Sugar * self.worldPop * self.scalingConst
+        # If this value is zero, it means that some value didn't get set.
+        if (n == 0.0):
+            print "\nError: fraction of land for sugar and world population"\
+                  +"must be set before computing estimate.\n"
+        return int(n)
+
+
+    ## Estimate the number of jelly beans in the world using the new pink data.
+    def compute_Njelly_pink_est(self):
+
+        n = self.fracLand4Sugar * self.worldPop * self.scalingConst / \
+            (1.0 - self.fracPplLovingPink)
+        # If this value is zero, it means that some value didn't get set.
+        if (n == 0.0):
+            print "\nError: fraction of land for sugar, world population, and"\
+                  +"fraction of people loving pink must be set before "\
+                  +"computing estimate.\n"
+        return int(n)
+
 
         # THW: Add a test for type here
  
@@ -96,5 +197,3 @@ class NumJellyEstimator:
         # THW: What other checks might be useful? What is a better way to do this?
 
         return int(n)
-
-
---
+++ readme.md
@@ -0,0 +1,2 @@
+Assignment is in Hw7.txt
+Documentation for Jelly Beans is in documentation/html/index.html
