529bac8b460cb94b405b4e7d65a2322bcca1ee3e
gtrichardson <gtrichardson@berkeley.edu>
2016-04-10 12:09:17 -0700

    the commented version for hw

---
+++ documentation/EstNumJellyBeansComments (1).py
@@ -0,0 +1,109 @@
+#!/c/Users/bob/Anaconda3/python
+
+import sys
+
+## This class estimates the number of jelly beans in the world using input data
+# determined to be correlated to this result.
+# The number of jelly beans in the world is correlated to the fraction
+# of land used for sugar, the world population, and the fraction of 
+# people who like the color pink.
+class NumJellyEstimator:
+
+    ## Instantiating the class initializes some variables.
+    def __init__(self):
+
+        ## Fraction of land used for growing sugar
+        self.fracLand4Sugar = 0.0
+        ## World population
+        self.worldPop = 0
+        ## Scaling constant used in estimate
+        self.scalingConst = 1e-1
+        ## Fraction of people who love the color pink.
+        self.fracPplLovingPink = 0.0
+
+
+    ## Set the fraction of land used for sugar.
+    # \param frac fraction of land used for sugar (float between 0 and 1)
+    def set_land_frac_for_sugar(self, frac):
+
+        # Make sure we've got a float.
+        assert type(frac) is float, \
+            "Error: fraction of land set must be a float."
+
+        # Check that the value is between zero and one.
+        if ((frac <= 0.0) or (frac >= 1.0)):
+            print "\nError: Fraction of land used for sugar must be between"\
+                  +" 0.0 and 1.0.\n"
+            sys.exit()
+
+        # Store the fraction.
+        self.fracLand4Sugar = frac
+
+
+    ## Set the world population
+    # \param people integer number of people on earth
+    def set_world_pop(self, people):
+
+        assert type(people) is int
+            "Error: Only entire people may exist, so far at least"
+ 
+        if people > 7000000000:
+            raise Exception ("There are not that many people in the world")
+            
+        
+
+        # Store the number of people.
+        self.worldPop = people
+
+
+    ## Set the fraction of people who love the color pink.
+    # \param frac float less than one
+    def set_frac_ppl_loving_pink(self, frac):
+
+        # Fraciton, in this case, means a decimal verison of percentage.
+        assert type(frac) is float  
+            "Error: You broke math. Please correct it"
+
+        # In all honesty, it cant be greater than 1.0, but hey, this is fun.
+        if frac > 0.75:
+            raise exception ("I dont think that's true. Everyone knows blue is the best color")
+
+        # Store the fraction.
+        self.fracPplLovingPink = frac
+
+
+    ## Return the scaling constant so the user can check it if they want.
+    # The scaling constant was set in the __init__ fuction at the begining
+    def get_scaling_const(self):
+
+        return self.scalingConst
+
+
+    ## Estimate the number of jelly beans in the world.
+    # This is based on a previous understanding of the estimate that did not
+    # take the color pink into account. Still supported for legacy reasons.
+    def compute_Njelly_est_nopink(self):
+
+        n = self.fracLand4Sugar * self.worldPop * self.scalingConst
+        # If this value is zero, it means that some value didn't get set.
+        if (n == 0.0):
+            print "\nError: fraction of land for sugar and world population"\
+                  +"must be set before computing estimate.\n"
+        return int(n)
+
+
+    ## Estimate the number of jelly beans in the world using the new pink data.
+    def compute_Njelly_est(self):
+
+        n = self.fracLand4Sugar * self.worldPop * self.scalingConst / \
+            (1.0 - self.fracPplLovingPink)
+        # If this value is zero, it means that some value didn't get set.
+        if (n == 0.0):
+            print "\nError: fraction of land for sugar, world population, and"\
+                  +"fraction of people loving pink must be set before "\
+                  +"computing estimate.\n"
+
+
+        return int(n)
+
+
